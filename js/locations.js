0b0be2c7391c41a32c5866cb9f3e9b185831964a
test <tpierag1@binghamton.edu>
2017-11-13 22:43:50 -0500

    Kinda Sorta Works, Wireframed

---
+++ js/locations.js
@@ -0,0 +1,37 @@
+//Data for model//
+
+var locations = [
+    {
+        title: 'Guide to Kulchur',
+        location: {
+            lat: 41.477217,
+            lng: -81.722749
+        }
+    },
+    {
+        title: "Happy Dog",
+        location: {
+            lat: 41.484881,
+            lng: -81.726483
+        }
+    },
+    {
+        title: "Bop Stop",
+        lat: 41.489878,
+        lng: -81.711737
+    },
+    {
+        title: "Now That's Class",
+        lat: 41.482600,
+        lng: -81.765594
+    },
+    {
+        title: "Mahalls",
+        lat: 41.477095,
+        lng: -81.781151
+    },
+    {
+        title: "SPACES",
+        lat: 41.489854,
+        lng: -81.711343
+    }]
